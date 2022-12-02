b3999a3c0cd7a63d5fbbd12612d1a524e5e4369a
NahuRodas <cesarnahuelrodas@gmail.com>
2022-12-02 15:50:12 -0300

    2/12

--- codigo.js
+++ codigo.js
@@ -7,7 +7,7 @@ var imagenes = [ html = document.getElementById('html'),
     bootsrap = document.getElementById('bootstrap'), 
     java = document.getElementById('java'),  
     cs = document.getElementById('c#'), 
-    cp = document.getElementById('cpp'),    
+    cp = document.getElementById('c++'),    
     php = document.getElementById('php'),   
     python = document.getElementById('python'), 
     mysql = document.getElementById('mysql'),   
@@ -16,20 +16,22 @@ var imagenes = [ html = document.getElementById('html'),
     wordPress = document.getElementById('wordPress'),  
     afterEffects = document.getElementById('afterEffects'),  
     photoshop = document.getElementById('photoshop') ];
-
+    var elemento;
+    var padre;
 
 imagenes.forEach(e => {
     e.addEventListener('mouseover',function(){
         e.src = e.src.replace("G","");
-        var padre;
         padre = e.parentNode;
-        padre.createElement("h1");
-        var contenido = e.id;
-        
+        elemento = document.createElement('h1');
+        elemento.className = "logo";
+        elemento.innerHTML = e.id;
+        padre.append(elemento);
 
+        
     })
     e.addEventListener('mouseout',function(){
         e.src = e.src.slice(0,-4)+"G.png";
-    })
+        padre.removeChild(padre.lastChild);    })
 });
 
